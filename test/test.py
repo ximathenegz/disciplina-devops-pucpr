32456cddf3b25ede41da4d196aea42d8a23b25c0
Henrique Ximenes <beingxima@gmail.com>
2026-04-26 21:27:26 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,40 +4,36 @@ from unittest.mock import patch
 
 def test_root():
     result = root()
-    yield result
     assert result == {"message": " Hello World"}
 
-def test_funcaoteste():
-    with patch('random.randint', return_value=12345):
-        result = funcaoteste()
-        yield result
 
+def test_funcaoteste():
+    with patch('src.main.random.randint', return_value=12345):
+        result = funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
-def test_create_estudante(estudante: Estudante):
+def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="curso 1", ativo=False)
     result = create_estudante(estudante_teste)
-    yield result
     assert estudante_teste == result
 
 
-def test_update_estudante_negativo(id_estudante: int):
+def test_update_estudante_negativo():
     result = update_estudante(-5)
-    yield result
     assert not result
 
-def test_update_estudante_positivo(id_estudante: int):
+
+def test_update_estudante_positivo():
     result = update_estudante(10)
-    yield result
     assert result
-def test_delete_estudante_negativo(id_estudante: int):
+
+
+def test_delete_estudante_negativo():
     result = delete_estudante(-5)
-    yield result
     assert not result
 
-def test_delete_estudante_positivo(id_estudante: int):
-    result = delete_estudante(5)
-    yield result
-    assert result
 
+def test_delete_estudante_positivo():
+    result = delete_estudante(5)
+    assert result
